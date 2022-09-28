8db495c1ae84777ca547f4c2ee55a3b5f755c46f
Manideep <17101992m@gmail.com>
2022-09-28 23:47:55 +0530

    comment added to the script file

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+// Will get input from user
 const textArea = document.querySelector("textarea");
+
+// Count update
 const wordCount = document.querySelector(".wordCount");
 const charCount = document.querySelector(".charCount");
 
 textArea.addEventListener('input', (e) => {
+    // will get the value on each input
     let charNow = e.target.value;
 
+    // will trim whitespaces from start and end
     let trim = charNow.trim();
 
-
+    // This will remove whitespaces between chars, join them, calculate length and update on the page
     let charTotal = trim.split(" ").join('')
     let charLength = charTotal.length;
     charCount.textContent = charLength;
 
+    // This will remove whitespaces between chars
     let wordsArr = trim.split(" ");
+
+    // If first word is blank remove it
     if(wordsArr[0] === "") wordsArr.splice(0, 1);
-    console.log(wordsArr)
+
+    // Calculate the length and update on the page.
     let wordLength = wordsArr.length;
     wordCount.textContent = wordLength;
 })
